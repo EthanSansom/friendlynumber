f1a0372b80a17aee56a7dbee66a6a182e427ceff
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-03-23 18:18:01 -0400

    Add a `format_fractional()` method for formatting the decimal part of a number (e.g. `0.1234000` -> "0.1234").

--- R/format.R
+++ R/format.R
@@ -32,10 +32,62 @@ format_whole.default <- function(x, ...) {
   stop(paste0("No `format_whole()` method implemented for class <", class(x)[[1]], ">."))
 }
 
-# decimal ----------------------------------------------------------------------
+# fractional -------------------------------------------------------------------
 
-# How to format a "decimal" number
+# How to format a fractional number in range (0, 1)
 #' @export
-format_whole <- function(x, ...) {
-  UseMethod("format_whole")
+format_fractional <- function(x, ...) {
+  UseMethod("format_fractional")
 }
+
+#' @export
+format_fractional.numeric <- function(x) {
+  sub("0+$", "", sprintf("%.15f", x))
+}
+
+#' @export
+format_fractional.bignum_bigfloat <- function(x) {
+  sub("0+$", "", format(
+    x,
+    notation = "dec",
+    digits = getOption("friendlynumber.bigfloat.digits")
+  ))
+}
+
+#' @export
+format_fractional.default <- function(x, ...) {
+  stop(paste0("No `format_fractional()` method implemented for class <", class(x)[[1]], ">."))
+}
+
+# Test whether to use `format()` or `sprintf()`
+if (FALSE) {
+  format_1 <- function(x) {
+    format(x, digits = 16, scientific = FALSE)
+  }
+  format_2 <- function(x) {
+    # sub("0+$", "", sprintf("%.16f", x))
+    sprintf("%.16f", x)
+  }
+
+  # 18 characters ("0." + 16 digits)
+  format_1(1/3) |> nchar()
+  format_2(1/3) |> nchar()
+
+  # NOTE: The `digits` in `format()` seems like more of a suggestion, and reading
+  # do docs it seems like that's the case. We're going to want something more
+  # consistent.
+  set.seed(123)
+  x <- runif(1000)
+  format_1(c(1/3, x)) |> nchar() |> unique() # 21 characters, unexpected
+  format_2(c(1/3, x)) |> nchar() |> unique() # 18 characters, as expected
+
+  # `sprintf()` is faster anyways
+  bench::mark(
+    format_1(x),
+    format_2(x),
+    check = FALSE
+  )
+
+}
+
+
