4f92d2d0d2116c7633a5c02ec84fa6d01c379244
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-03-23 15:58:55 -0400

    Remove redundant `format()` call

--- R/nth-friendly.R
+++ R/nth-friendly.R
@@ -8,7 +8,8 @@ nth_friendly <- function(
     bigmark = TRUE
   ) {
 
-  out <- paste0(format(numbers, big.mark = if (bigmark) "," else "", scientific = FALSE), "th")
+  # TODO: Maybe needs something for <biginteger>
+  out <- paste0(format_whole(numbers, bigmark = if (bigmark) "," else ""), "th")
 
   infinites <- is.infinite(numbers)
   missings <- is.na(numbers)
@@ -29,38 +30,18 @@ nth_friendly <- function(
     return(out)
   }
 
-  # TODO: Maybe needs something for <biginteger>
-  englishified <- paste0(format(
-    x = numbers[needs_englishifying],
-    # Allowing `big.mark` to be specified would complicate the regex
-    big.mark = if (bigmark) "," else "",
-    scientific = FALSE
-  ), "th")
-
   # Special rules for word endings:
   # 1     -> 1st (but not 11 -> 11st)
   # 2     -> 2nd
   # 3     -> 3rd
-  out[needs_englishifying] <- sub("3th$", "3rd", sub("2th$", "2nd", sub("(?<!1)1th$", "1st", englishified, perl = TRUE)))
+  out[needs_englishifying] <- sub("3th$", "3rd", sub("2th$", "2nd", sub("(?<!1)1th$", "1st", out[needs_englishifying], perl = TRUE)))
   trimws(out)
 }
 
-english_nth_friendly <- function(numbers, bigmark = TRUE) {
-  if (length(numbers) == 0) {
-    return(character())
-  }
 
-  # Allowing `big.mark` to be specified would complicate the regex
-  big.mark <- if (bigmark) "," else ""
-  english_ordinals <- paste0(format(numbers, big.mark = big.mark, scientific = FALSE), "th")
+# We'll need a different method for <biginteger>
+# format(bignum::biginteger("1000000000000"), big.mark = ",")
 
-  # Special rules for word endings:
-  # 1     -> 1st (but not 11 -> 11st)
-  # 2     -> 2nd
-  # 3     -> 3rd
-  trimws(
-    sub(
-      "3th$", "3rd",
-      sub("2th$", "2nd",
-          sub("(?<!1)1th$", "1st", english_ordinals, perl = TRUE))))
-}
+
+
+
