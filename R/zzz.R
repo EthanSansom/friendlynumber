c33a7804d6311b11fc1f13281514549c8c114486
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-03-24 17:15:47 -0400

    Lower the default digits to 7, since at 15 we start to see some serious precision problems

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,8 @@
 .onLoad <- function(libname, pkgname) {
   opts <- options()
   friendlynumber_opts <- list(
-    friendlynumber.bigfloat.digits = 15
+    friendlynumber.numeric.digits = 7,
+    friendlynumber.bigfloat.digits = 7
   )
   set_opts_at <- !(names(friendlynumber_opts) %in% names(opts))
   if (any(set_opts_at)) options(friendlynumber_opts[set_opts_at])
