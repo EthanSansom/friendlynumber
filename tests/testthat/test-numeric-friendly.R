ea737eb086c3835f6d786ec7c2633247fdc9b073
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-03-25 17:25:55 -0400

    More unit tests

--- tests/testthat/test-numeric-friendly.R
+++ tests/testthat/test-numeric-friendly.R
@@ -1,18 +1,36 @@
-test_that("`numeric_friendly.numeric() works", {
+test_that("`numeric_friendly()` works", {
   expect_equal(
     numeric_friendly(c(-Inf, Inf, 0, NA, NaN)),
     c("negative infinity", "infinity", "zero", "missing", "not a number")
   )
+  expect_equal(
+    numeric_friendly(
+      c(-Inf, Inf, 0, NA, NaN),
+      negative = "-",
+      inf = "Inf",
+      zero = "0",
+      na = "NA",
+      nan = "NaN"
+    ),
+    paste(c(-Inf, Inf, 0, NA, NaN))
+  )
+  expect_equal(
+    numeric_friendly(c(1, 1.001, 0.001, 0.002, 10.002, 1001, 999999, 999.123)),
+    c(
+      "one",
+      "one and one thousandth",
+      "one thousandth",
+      "two thousandths",
+      "ten and two thousandths",
+      "one thousand one",
+      "nine hundred ninety-nine thousand nine hundred ninety-nine",
+      "nine hundred ninety-nine and one hundred twenty-three thousandths"
+    )
+  )
+})
 
-  # TODO: Test negatives, and just put all of these in one `expect_equal()`
-  expect_equal(numeric_friendly(1), "one")
-  expect_equal(numeric_friendly(1.001), "one and one thousandth")
-  expect_equal(numeric_friendly(0.001), "one thousandth")
-  expect_equal(numeric_friendly(0.002), "two thousandths")
-  expect_equal(numeric_friendly(10.002), "ten and two thousandths")
-  expect_equal(numeric_friendly(1001), "one thousand one")
-  expect_equal(numeric_friendly(999999), "nine hundred ninety-nine thousand nine hundred ninety-nine")
-  expect_equal(numeric_friendly(999.123), "nine hundred ninety-nine and one hundred twenty-three thousandths")
+test_that("`numeric_friendly()` returns `character(0)` on zero-length input", {
+  expect_identical(numeric_friendly(numeric()), character())
 })
 
 test_that("Nice fractions (e.g. 'one half') work", {
@@ -32,29 +50,41 @@ test_that("Nice fractions (e.g. 'one half') work", {
 test_that("`and` and `and_fractional` arguments work", {
   # `and` (`and_fractional = and` by default)
   expect_equal(
-    numeric_friendly(c(1, 1001, 1021), and = TRUE),
-    c("one", "one thousand and one", "one thousand and twenty-one")
+    numeric_friendly(c(1, 1001, 1021, 1.1, 1021.1, 1021.1021), and = TRUE, decimal = " point "),
+    c(
+      "one",
+      "one thousand and one",
+      "one thousand and twenty-one",
+      "one point one tenth",
+      "one thousand and twenty-one point one tenth",
+      "one thousand and twenty-one point one thousand and twenty-one ten thousandths"
+    )
   )
   expect_equal(
-    numeric_friendly(c(1, 1001, 1021), and = FALSE),
-    c("one", "one thousand one", "one thousand twenty-one")
+    numeric_friendly(c(1, 1001, 1021, 1.1, 1021.1, 1021.1021), and = FALSE, decimal = " point "),
+    c(
+      "one",
+      "one thousand one",
+      "one thousand twenty-one",
+      "one point one tenth",
+      "one thousand twenty-one point one tenth",
+      "one thousand twenty-one point one thousand twenty-one ten thousandths"
+    )
   )
   expect_equal(
-    numeric_friendly(c(1, 1001, 1021), and = FALSE),
-    c("one", "one thousand one", "one thousand twenty-one")
-  )
-
-  expect_equal(
-    numeric_friendly(c(1, 1001, 1021), and = FALSE),
-    c("one", "one thousand one", "one thousand twenty-one")
+    numeric_friendly(1021.1021, and = TRUE, and_fractional = FALSE, decimal = " point "),
+    "one thousand and twenty-one point one thousand twenty-one ten thousandths"
   )
 })
 
 test_that("`decimal` argument works", {
+  expect_equal(numeric_friendly(1, decimal = "-"), "one")
+  expect_equal(numeric_friendly(0.1, decimal = "-"), "one tenth")
   expect_equal(numeric_friendly(1.1, decimal = "-"), "one-one tenth")
 })
 
 test_that("Regex metacharacters in `decimal` are handled correctly", {
   expect_equal(numeric_friendly(1.1, decimal = "\\1"), "one\\1one tenth")
+  expect_equal(numeric_friendly(1, decimal = "\\1"), "one")
   expect_equal(numeric_friendly(1.1, decimal = "$"), "one$one tenth")
 })
