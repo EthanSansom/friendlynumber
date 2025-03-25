e2de56a4f8a1fe7483fca4febcac60650d8dce02
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-03-25 17:33:05 -0400

    Update options

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   opts <- options()
-  friendlynumber_opts <- list(
-    friendlynumber.numeric.digits = 7,
-    friendlynumber.bigfloat.digits = 7
-  )
+  friendlynumber_opts <- friendlynumber_default_options()
   set_opts_at <- !(names(friendlynumber_opts) %in% names(opts))
   if (any(set_opts_at)) options(friendlynumber_opts[set_opts_at])
   invisible()
