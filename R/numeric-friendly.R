84db0d8ea64269fb9d2c29b4ab841a81c452f93f
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-03-25 17:27:10 -0400

    All no-whitespace `negative` argument (e.g. `negative = "-"` formats as "-one")

--- R/numeric-friendly.R
+++ R/numeric-friendly.R
@@ -1,36 +1,20 @@
 # functions --------------------------------------------------------------------
 
-# TODO: Implement this by splitting apart the fractional and whole parts. Pass
-# the whole part to `integer_friendly()` and the fractional part can be constructed
-# utilizing `format_fractional()` and `english_fractionals()`. You'll want to define
-# some `english$fractions <- c(`0.5` = "a half")` and use those for the common cases.
-#
-# Also allow them to provide the `english_fractions` as a named character vector,
-# which overrides these defaults. A package implementer can wrap that in an option
-# or something.
 numeric_friendly <- function(
     numbers,
     zero = "zero",
     na = "missing",
     nan = "not a number",
     inf = "infinity",
-    negative = "negative",
+    negative = "negative ",
     decimal = " and ",
     and = FALSE,
     hyphenate = TRUE,
     and_fractional = and,
     hyphenate_fractional = hyphenate,
-    english_fractions = get_english_fractions()
+    english_fractions = NULL
   ) {
 
-  # NOTE: No, this doesn't yield any speedup.
-  #
-  # RESOLVED: Is there a potential speedup if we just let this be one of the common
-  # cases? Like we could initialize with `rep_len(zero, length(numbers))` and
-  # then not do `out[zeros] <-`.
-  #
-  # We're doing a lot of zero imputing, I think that would be helpful so long
-  # as `character(length(numbers))` and `rep_len()` are similar speeds.
   out <- character(length(numbers))
 
   infinites <- is.infinite(numbers)
@@ -49,7 +33,7 @@ numeric_friendly <- function(
 
   needs_englishifying <- !(infinites | missings | zeros)
   if (!any(needs_englishifying)) {
-    out[negatives] <- paste(negative, inf)
+    out[negatives] <- paste0(negative, inf)
     return(out)
   }
 
@@ -66,10 +50,17 @@ numeric_friendly <- function(
         and = and,
         hyphenate = hyphenate
       )
-      out[negatives] <- paste(negative, out[negatives])
+      out[negatives] <- paste0(negative, out[negatives])
       return(out)
     }
     return(english_naturals(remaining_wholes, and = and, hyphenate = hyphenate))
+  }
+
+  # This isn't required until now
+  if (is.null(english_fractions)) {
+    # `remaining_numbers[1]^0` provides a `1` of the correct class (e.g. a
+    # <numeric> or <bignum_bigfloat>).
+    english_fractions <- get_english_fractions(one = remaining_numbers[1]^0)
   }
 
   remaining_fractionals <- remaining_numbers - remaining_wholes
@@ -102,8 +93,8 @@ numeric_friendly <- function(
     "<decimal>",
     english_fractionals(
       remaining_fractionals[is_compound],
-      and = and,
-      hyphenate = hyphenate,
+      and = and_fractional,
+      hyphenate = hyphenate_fractional,
       zero = "<fractional zero>", # Later removed
       english_fractions = english_fractions
     )
