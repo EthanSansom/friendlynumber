f720417bc9ea88e50f25941bc4571913a09265cf
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-03-25 15:33:52 -0400

    Add `english_fractions` argument and fix big occuring when `decimal` is a regex metacharacter (e.g. `\\1`, `$`)

--- R/numeric-friendly.R
+++ R/numeric-friendly.R
@@ -1,11 +1,3 @@
-# todos ------------------------------------------------------------------------
-
-# TODO: Before we venture too far down the rabbit hole, let's get some unit tests
-# going for all of these things. `numeric_friendly()` and `integer_friendly()`
-# and `ordinal_friendly()` in particular.
-
-# TODO: `scalar_numeric_friendly()`, take advantage of the scalar-ness
-
 # functions --------------------------------------------------------------------
 
 # TODO: Implement this by splitting apart the fractional and whole parts. Pass
@@ -27,7 +19,8 @@ numeric_friendly <- function(
     and = FALSE,
     hyphenate = TRUE,
     and_fractional = and,
-    hyphenate_fractional = hyphenate
+    hyphenate_fractional = hyphenate,
+    english_fractions = get_english_fractions()
   ) {
 
   # NOTE: No, this doesn't yield any speedup.
@@ -60,17 +53,29 @@ numeric_friendly <- function(
     return(out)
   }
 
-  remaining_numbers <- numbers[needs_englishifying]
+  remaining_numbers <- abs(numbers[needs_englishifying])
   remaining_wholes <- trunc(remaining_numbers)
-  remaining_fractionals <- remaining_numbers - remaining_wholes
 
+  # TODO: These only actually need to be equal up to a certain precision, might
+  # be better to do `remaining_numbers - remaining_wholes < get_epsilon()`, or
+  # we could round `remaining_numbers` to the correct digits first.
+  if (all(remaining_numbers == remaining_wholes)) {
+    if (all(remaining_wholes < 1000)) {
+      out[needs_englishifying] <- after_format(
+        english_hundreds(remaining_wholes),
+        and = and,
+        hyphenate = hyphenate
+      )
+      out[negatives] <- paste(negative, out[negatives])
+      return(out)
+    }
+    return(english_naturals(remaining_wholes, and = and, hyphenate = hyphenate))
+  }
+
+  remaining_fractionals <- remaining_numbers - remaining_wholes
   is_whole <- remaining_fractionals == 0
   is_fractional <- remaining_wholes == 0
   is_compound <- !is_whole & !is_fractional
-
-  # TODO: Is there a speed difference if we do both naturals and fractions for
-  # every element of `out[needs_englishifying]` and then drop the whole or
-  # fractional portion as needed?
 
   out[needs_englishifying][is_whole] <- english_naturals(
     remaining_wholes[is_whole],
@@ -81,23 +86,57 @@ numeric_friendly <- function(
     remaining_fractionals[is_fractional],
     and = and_fractional,
     hyphenate = hyphenate_fractional,
-    zero = zero
+    zero = zero,
+    english_fractions = english_fractions
   )
 
   out[needs_englishifying][is_compound] <- paste0(
-    english_naturals(remaining_wholes[is_compound], and = and, hyphenate = hyphenate),
-    decimal,
-    english_fractionals(remaining_fractionals[is_compound], and = and, hyphenate = hyphenate, zero = zero)
+    english_naturals(
+      remaining_wholes[is_compound],
+      and = and,
+      hyphenate = hyphenate
+    ),
+    # Later subbed out for `decimal`. We check for trailing fractional zero's
+    # (resulting from insufficient precision) using `sub()`, which errors if
+    # `decimal` is a regex metacharacter.
+    "<decimal>",
+    english_fractionals(
+      remaining_fractionals[is_compound],
+      and = and,
+      hyphenate = hyphenate,
+      zero = "<fractional zero>", # Later removed
+      english_fractions = english_fractions
+    )
+  )
+  out[needs_englishifying][is_compound] <- sub(
+    # First remove any trailing fractional zeros
+    x = sub(
+      x = out[needs_englishifying][is_compound],
+      pattern = "<decimal><fractional zero>$",
+      replacement = ""
+    ),
+    # Then, insert the user supplied decimal. Note that `fixed = TRUE` here
+    # prevents user-supplied metacharacters from breaking things.
+    pattern = "<decimal>",
+    replacement = decimal,
+    fixed = TRUE
   )
 
-  # TODO: We might have to be more careful about this `sub()`, since `decimal`
-  # or `zero` might be control sequences. Look into how you're supposed to do
-  # regular expressions containing a user output. I think an easy thing to do
-  # is to add my own trailing symbol (e.g. "-") that we can use to detect
-  # fractional zeros.
-
-  # Removes trailing zeros (e.g. " and zero") which occur when we don't have
-  # enough precision to format small `remaining_fractionals`.
-  out <- sub(paste0(decimal, zero, "$"), "", out)
   out
+}
+
+# Which is faster, `all(x == y)` or `all.equal(x, y)` (looks like `all()` wins)
+if (FALSE) {
+
+  ints <- ints_off <- 1:10000
+  ints_off[10000] <- 0
+  bench::mark(
+    all(ints == ints),
+    all.equal(ints, ints)
+  )
+  bench::mark(
+    all(ints == ints_off),
+    isTRUE(all.equal(ints, ints_off))
+  )
+
 }
