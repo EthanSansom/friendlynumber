5913cdfee29eed2f4355a21ee0ddbaabfde3e150
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-03-27 19:27:34 -0400

    Add `_safe()`. Fix bug where `negative` wasn't being applied. Fix bug causing 12, 13 -> "12nd" and "13rd"

--- R/nth-friendly.R
+++ R/nth-friendly.R
@@ -1,18 +1,14 @@
-# TODO: I think this should expect positive integerish numbers, (e.g. 0, 1, 2, ...),
-# as should all of the `n*_friendly` functions.
-
 nth_friendly <- function(
     numbers,
     zero = "zeroth",
     na = "missingth",
     nan = "not a numberth",
-    inf = "infinityth",
-    negative = "negative",
+    inf = "infinitieth",
+    negative = "negative ",
     bigmark = TRUE
   ) {
 
-  # TODO: Maybe needs something for <biginteger>
-  out <- paste0(format_whole(numbers, bigmark = if (bigmark) "," else ""), "th")
+  out <- character(length(numbers))
 
   infinites <- is.infinite(numbers)
   missings <- is.na(numbers)
@@ -25,18 +21,52 @@ nth_friendly <- function(
   out[nas] <- na
   out[nans] <- nan
   out[zeros] <- zero
-  out[negatives] <- paste(negative, out[negatives])
 
   needs_englishifying <- !(infinites | missings | zeros)
   if (!any(needs_englishifying)) {
-    out[negatives] <- paste(negative, inf)
+    out[negatives] <- paste0(negative, inf)
     return(out)
   }
 
   # Special rules for word endings:
+  # *     -> *th
   # 1     -> 1st (but not 11 -> 11st)
-  # 2     -> 2nd
+  # 2     -> 2nd (but not 12 -> 12nd)
   # 3     -> 3rd
-  out[needs_englishifying] <- sub("3th$", "3rd", sub("2th$", "2nd", sub("(?<!1)1th$", "1st", out[needs_englishifying], perl = TRUE)))
+  out[needs_englishifying] <- format_whole(abs(numbers[needs_englishifying]), bigmark = if (bigmark) "," else "")
+  out[needs_englishifying] <- sub("(?<!1)3th$", "3rd",
+                                  sub("(?<!1)2th$", "2nd",
+                                      sub("(?<!1)1th$", "1st",
+                                          paste0(out[needs_englishifying], "th"), perl = TRUE), perl = TRUE), perl = TRUE)
+
+  out[negatives] <- paste0(negative, out[negatives])
   trimws(out)
+}
+
+nth_friendly_safe <- function(
+    numbers,
+    zero = "zeroth",
+    na = "missingth",
+    nan = "not a numberth",
+    inf = "infinitieth",
+    negative = "negative ",
+    bigmark = TRUE
+) {
+  numbers <- check_is_whole(numbers)
+  zero <- check_is_string(zero)
+  na <- check_is_string(na)
+  nan <- check_is_string(nan)
+  inf <- check_is_string(inf)
+  negative <- check_is_string(negative)
+  bigmark <- check_is_bool(bigmark)
+
+  nth_friendly(
+    numbers = numbers,
+    zero = zero,
+    na = na,
+    nan = nan,
+    inf = inf,
+    negative = negative,
+    bigmark = bigmark
+  )
 }
