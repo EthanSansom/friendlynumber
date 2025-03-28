45aacb1e0ed3eaa7d3588e74250aa68f40c2f150
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-03-28 08:49:07 -0400

    Add `format_number()` unit tests for integer, bigfloat, biginteger

--- tests/testthat/test-format.R
+++ tests/testthat/test-format.R
@@ -3,20 +3,50 @@ test_that("format_number.numeric() works", {
     format_number(c(NaN, NA, Inf, -Inf, 0, 0.0)),
     c("NaN", "NA", "Inf", "-Inf", "0", "0")
   )
-  expect_equal(format_number(123), "123")
-  expect_equal(format_number(1234), "1,234")
-  expect_equal(format_number(1234.0), "1,234")
-  expect_equal(format_number(1234.5), "1,234.5")
-  expect_equal(format_number(1234.567), "1,234.567")
-  expect_equal(format_number(0.1), "0.1")
-  expect_equal(format_number(0.123), "0.123")
-  expect_equal(format_number(0.1234), "0.1234")
+  expect_equal(
+    format_number(c(123, 1234, 1234.0, 1234.5, 1234.567, 0.1, 0.123, 0.1234)),
+    c("123", "1,234", "1,234", "1,234.5", "1,234.567", "0.1", "0.123", "0.1234")
+  )
+  expect_equal(format_number(-c(123, 0.1234)), c("-123", "-0.1234"))
 
   withr::local_options(list(friendlynumber.numeric.digits = 2))
-  expect_equal(format_number(1234.0), "1,234")
-  expect_equal(format_number(1234.5), "1,234.5")
-  expect_equal(format_number(1234.567), "1,234.57") # Rounded
-  expect_equal(format_number(0.1), "0.1")
-  expect_equal(format_number(0.123), "0.12")
-  expect_equal(format_number(0.1234), "0.12")
+  expect_equal(
+    format_number(c(1234.0, 1234.5, 1234.567, 0.1, 0.123, 0.1234)),
+    c("1,234", "1,234.5", "1,234.57", "0.1", "0.12", "0.12")
+  )
+})
+
+test_that("format_number.integer() works", {
+  expect_equal(format_number(c(NA_integer_, 0L)), c("NA", "0"))
+  expect_equal(format_number(c(1L, 123L, 1234L)), c("1", "123", "1,234"))
+  expect_equal(format_number(-c(1L, 123L, 1234L)), c("-1", "-123", "-1,234"))
+})
+
+test_that("format_number.bignum_bigfloat() works", {
+  skip_if_not_installed("bignum")
+
+  expect_equal(
+    format_number(bignum::bigfloat(c(NaN, NA, Inf, -Inf, 0, 0.0))),
+    c("NaN", "NA", "Inf", "-Inf", "0", "0")
+  )
+  expect_equal(
+    format_number(bignum::bigfloat(c(123, 1234, 1234.0, 1234.5, 1234.567, 0.1, 0.123, 0.1234))),
+    c("123", "1,234", "1,234", "1,234.5", "1,234.567", "0.1", "0.123", "0.1234")
+  )
+  expect_equal(format_number(bignum::bigfloat(-c(123, 0.1234))), c("-123", "-0.1234"))
+
+  withr::local_options(list(friendlynumber.bigfloat.digits = 2))
+  expect_equal(
+    format_number(bignum::bigfloat(c(1234.0, 1234.5, 1234.567, 0.1, 0.123, 0.1234))),
+    c("1,234", "1,234.5", "1,234.57", "0.1", "0.12", "0.12")
+  )
+  expect_equal(format_number(bignum::bigfloat(-c(123, 0.1234))), c("-123", "-0.12"))
+})
+
+test_that("format_number.bignum_biginteger() works", {
+  skip_if_not_installed("bignum")
+
+  expect_equal(format_number(bignum::biginteger(c(NA, 0, 0.0))), c("NA", "0", "0"))
+  expect_equal(format_number(bignum::biginteger(c(1, 123, 1234))), c("1", "123", "1,234"))
+  expect_equal(format_number(bignum::biginteger(-c(1, 123, 1234))), c("-1", "-123", "-1,234"))
 })
