1d9d854ff56aa2aa9eda3807ecf79348a5ca5004
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-03-29 09:33:39 -0400

    Add function documentation

--- R/biginteger-friendly.R
+++ R/biginteger-friendly.R
@@ -1,3 +1,48 @@
+#' Translate a biginteger to a cardinal character vector
+#'
+#' @description
+#'
+#' Convert a `<bignum_biginteger>` to a cardinal numeral (e.g. one, two, three).
+#'
+#' A [bignum::biginteger()] can store any integer (i.e. arbitrary precision),
+#' which is useful for manipulating numbers too large to be represented (accurately)
+#' in an `<integer>` or `<numeric>` vector.
+#'
+#' `biginteger_friendly_safe()` checks that all arguments are of the correct type
+#' and raises an informative error otherwise. `biginteger_friendly()` does not
+#' perform input validation to maximize its speed.
+#'
+#' @inheritParams params
+#'
+#' @param numbers `[bignum_biginteger]`
+#'
+#' A [bignum::biginteger()] vector to translate.
+#'
+#' @returns
+#'
+#' A non-NA character vector of the same length as `numbers`.
+#'
+#' @examples
+#' if (requireNamespace("bignum", quietly = TRUE)) {
+#'   biginteger_friendly(bignum::biginteger(c(0, 1, 2, NA, 10001)))
+#'
+#'   # Specify the translations of "special" numbers
+#'   biginteger_friendly(bignum::biginteger(-10), negative = "minus ")
+#'   biginteger_friendly(bignum::biginteger(NA), na = "unknown")
+#'
+#'   # Modify the output formatting
+#'   biginteger_friendly(bignum::biginteger(9999))
+#'   biginteger_friendly(bignum::biginteger(9999), and = TRUE)
+#'   biginteger_friendly(bignum::biginteger(9999), hyphenate = FALSE)
+#'
+#'   # Translate large numbers
+#'   large <- bignum::biginteger(10L)^1001L
+#'   biginteger_friendly(large)
+#'
+#'   # Input validation
+#'   try(biginteger_friendly_safe(1L))
+#' }
+#' @export
 biginteger_friendly <- function(
     numbers,
     zero = "zero",
@@ -22,6 +67,8 @@ biginteger_friendly <- function(
   )
 }
 
+#' @rdname biginteger_friendly
+#' @export
 biginteger_friendly_safe <- function(
     numbers,
     zero = "zero",
@@ -32,8 +79,8 @@ biginteger_friendly_safe <- function(
     and = FALSE,
     hyphenate = TRUE
 ) {
-  # Some paths don't evaluate every one of these arguments, so we need to force
-  # the checks here.
+  # Some paths of `integerish_friendly()` don't evaluate every one of these
+  # arguments, so we need to force the checks here.
   numbers <- check_is_class(numbers, is_biginteger, "bignum_biginteger")
   zero <- check_is_string(zero)
   na <- check_is_string(na)
