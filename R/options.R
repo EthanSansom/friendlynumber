3a15ab22f127f241ea98c6d8104a118c664a8b93
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-03-31 08:43:33 -0400

    Minor documentation fix

--- R/options.R
+++ R/options.R
@@ -1,9 +1,9 @@
-#' Get the default options set by the {friendlynumber} package
+#' Get the default options set by the friendlynumber package
 #'
 #' @description
 #'
-#' Returns a list of options provided to `options()` when the {friendlynumber}
-#' package is loaded. Options set prior to loading the {friendlynumber} package
+#' Returns a list of options provided to `options()` when the friendlynumber
+#' package is loaded. Options set prior to loading the friendlynumber package
 #' are not overwritten on load.
 #'
 #' @returns
