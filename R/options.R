55389bbfdb115e02782cb9fcedbaafe1a66f4697
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-03-31 08:28:35 -0400

    Export `friendlynumber_default_options()`

--- R/options.R
+++ R/options.R
@@ -1,6 +1,28 @@
+#' Get the default options set by the {friendlynumber} package
+#'
+#' @description
+#'
+#' Returns a list of options provided to `options()` when the {friendlynumber}
+#' package is loaded. Options set prior to loading the {friendlynumber} package
+#' are not overwritten on load.
+#'
+#' @returns
+#'
+#' A named list of options.
+#'
+#' @examples
+#' friendlynumber_default_options()
+#'
+#' # Reset the package options
+#' if (FALSE) {
+#'   options(friendlynumber_default_options())
+#' }
+#' @export
+# nocov start
 friendlynumber_default_options <- function() {
   list(
     friendlynumber.numeric.digits = 7,
     friendlynumber.bigfloat.digits = 7
   )
 }
+# nocov end
